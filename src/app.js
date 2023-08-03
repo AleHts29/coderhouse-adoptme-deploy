cb144018abd7372f66dcf6428ac77738a66761e6
Alejandro Huertas <ahuertas@fi.uba.ar>
2023-08-03 17:16:11 -0300

    Feature: Nueva vista de .hbs

--- src/app.js
+++ src/app.js
@@ -1,22 +1,33 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import handlebars from 'express-handlebars';
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+import __dirname from './utils.js';
+import viewRouter from './routes/view.router.js'
 
 const app = express();
-const PORT = process.env.PORT||9090;
+const PORT = process.env.PORT || 9090;
 const connection = mongoose.connect(process.env.MONGO_URL)
+// const connection = mongoose.connect('mongodb+srv://admin:admin@cluster0.8hkzesl.mongodb.net/clase45-adoptme-deploy-dev?retryWrites=true&w=majority')
 
+// Confi de handlebars
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/users',usersRouter);
-app.use('/api/pets',petsRouter);
-app.use('/api/adoptions',adoptionsRouter);
-app.use('/api/sessions',sessionsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/pets', petsRouter);
+app.use('/api/adoptions', adoptionsRouter);
+app.use('/api/sessions', sessionsRouter);
+app.use('/', viewRouter);
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
